2ce449fd7140170dda8c1f71332c540c49734db7
alex <tangyongjin@gmail.com>
2021-02-14 11:49:52 +0800

    apache2-php7-x 添加 mcrypt 扩展,bcmatch 扩展

--- apache2-php7-x/build.sh
+++ apache2-php7-x/build.sh
@@ -1,2 +1,1 @@
 docker build -f ./Dockerfile -t apache2-php7-x  .
-
--- apache2-php7-x/run.sh
+++ apache2-php7-x/run.sh
@@ -1,1 +1,3 @@
-docker run  -itd --rm    -p 8888:80  --name  debug  -v /tmp/html/:/var/www/html  apache2-php7-debug
+docker kill debug 
+docker rm debug
+docker run  -itd --rm    -p 8888:80  --name  debug  -v /tmp/html/:/var/www/html  apache2-php7-x
